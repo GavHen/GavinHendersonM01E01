8ff7c68e7681d131c22df3f32af7abc11062ed92
GavHen <144075440+GavHen@users.noreply.github.com>
2024-03-24 20:10:56 -0400

    Exercise 6.9

---
+++ Conversion.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Conversion {
+
+    /** Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    /** Convert from meters to feet */
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Feet\tMeters\t|\tMeters\tFeet");
+        System.out.println("----------------|-------------------");
+        for (int i = 1; i <= 10; i++) { /** display the feet to meter conversion*/
+            double feet = i;
+            double meters = footToMeter(feet);
+            double metersRounded = Math.round(meters * 1000.0) / 1000.0;
+            System.out.printf("%.1f\t%.3f\t|\t", feet, metersRounded);
+            
+            /** displays the meter to feet conversion */
+            double metersInput = i * 5 + 15;
+            double feetConverted = meterToFoot(metersInput);
+            double feetConvertedRounded = Math.round(feetConverted * 1000.0) / 1000.0;
+            System.out.printf("%.1f\t%.3f%n", metersInput, feetConvertedRounded);
+        }
+    }
+}
